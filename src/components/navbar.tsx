4ef5d00b8caeb616cb12f4bde356a2ed06802aa9
thelightofgod4 <kerem2002uslu2002@gmail.com>
2025-07-23 19:25:30 +0300

    safari güncellemesi 2

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -102,8 +102,8 @@ function Navbar() {
 
   return (
     <div className="fixed inset-x-0 top-0 bg-slate-100 z-[10] min-h-[64px] py-4 safe-top">
-      <div className="flex items-center justify-between h-full gap-2 px-4 md:px-8 mx-auto max-w-7xl min-h-[56px]">
-        <div className="flex flex-row gap-3 justify-center">
+      <div className="flex items-center justify-between h-full gap-2 px-4 md:px-8 w-full max-w-7xl min-h-[56px]">
+        <div className="flex flex-row gap-3">
           <Link href={"/dashboard"} className="flex items-center gap-2">
             <Video className="w-6 h-6 text-indigo-600" />
             <p className="px-2 py-1 text-lg md:text-2xl font-bold text-black">
